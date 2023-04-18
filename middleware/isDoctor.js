ff2d5a896b9368492d9cc115bb2229eb9276d45f
Luis Guerrero Roquett <luisguerreroroquett@gmail.com>
2023-04-18 17:43:16 +0200

    making middlewares

---
+++ middleware/isDoctor.js
@@ -0,0 +1,21 @@
+const { Doctors } = require("../models");
+const { sendErrorResponse } = require("../_util/sendResponse");
+const isDoctor = async (req, res, next) => {
+  try {
+    const doctor = await Doctors.findOne({
+      where: { id_usuario: req.user_id },
+    });
+
+    if (!doctor) {
+      return sendErrorResponse(res, 403, "Dont have permissions");
+    }
+
+    req.id_doctor = doctor.id;
+
+    next();
+  } catch (error) {
+    return sendErrorResponse(res, 500, "Error verifying user", error);
+  }
+};
+
+module.exports = isDoctor;
